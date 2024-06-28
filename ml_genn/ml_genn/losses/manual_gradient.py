bdd28390fc4f0154cbb3fa69b6a576734f0eb323
FabianSchubert <fj.schubert@freenet.de>
2024-06-28 17:37:03 +0100

    Add manual loss class and integrate into eventprop compiler, still needs work.

---
+++ ml_genn/ml_genn/losses/manual_gradient.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from .loss import Loss
+from ..utils.model import NeuronModel
+
+from typing import Callable
+
+from jax import grad
+
+
+class ManualGradient(Loss):
+    def __init__(self, loss_func: Callable):
+        self.loss = loss_func
+        # the provided loss function should take the input/predictions as the first argument.
+        self.grad_loss = grad(loss_func)
+
+    def set_target(
+        self,
+        genn_pop,
+        y_true: np.ndarray,
+        shape: tuple,
+        batch_size: int,
+        example_timesteps: int,
+    ):
+        expected_shape = (batch_size, example_timesteps) + shape
+
+        y_true = np.asarray(y_true)
+
+        assert (
+            y_true.shape == expected_shape
+        ), f"Expected shape {expected_shape}, got {y_true.shape} for y_true"
+
+        genn_pop.extra_global_params["RingV"].pull_from_device()
+
+        voltages = np.reshape(
+            genn_pop.extra_global_params["RingV"].view[:], expected_shape
+        )
+
+        gradients = self.grad_loss(voltages, y_true)
+
+        genn_pop.extra_global_params["Gradient"].view[:] = gradients.flatten()
+        genn_pop.extra_global_params["Gradient"].push_to_device()
