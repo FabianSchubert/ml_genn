bug fixes for manual gradient loss, add var_trial readout

ml_genn/ml_genn/compilers/event_prop_compiler.py
@@ -26,7 +26,7 @@ from ..connection import Connection
 from ..losses import Loss, SparseCategoricalCrossentropy, ManualGradient
 from ..neurons import Input, LeakyIntegrate, LeakyIntegrateFire, LeakyIntegrateFireInput
 from ..optimisers import Optimiser
-from ..readouts import Var, AvgVar, AvgVarExpWeight, MaxVar, SumVar
+from ..readouts import Var, VarTrial, AvgVar, AvgVarExpWeight, MaxVar, SumVar
 from ..synapses import Exponential
 from ..utils.callback_list import CallbackList
 from ..utils.data import MetricsType
@@ -523,6 +523,8 @@ class EventPropCompiler(Compiler):
                 max_time_required=True,
                 dt=self.dt,
                 example_timesteps=self.example_timesteps,
+                batch_size=self.batch_size,
+                pop_shape=pop.shape,
             )
 
             if sce_loss:
@@ -580,7 +582,7 @@ class EventPropCompiler(Compiler):
                     model_copy.add_var("RingWriteOffset", "int", 0)
                     model_copy.add_var("RingReadOffset", "int", 0)
 
-                    if loss_type is SparseCategoricalCrossentropy:
+                    if sce_loss:
                         # Add EGP for softmax V ring variable
                         ring_size = (
                             self.batch_size
@@ -605,11 +607,12 @@ class EventPropCompiler(Compiler):
                             * self.example_timesteps
                         )
 
-                        model_copy.add_egp(
-                            "RingV",
-                            "scalar*",
-                            np.empty(ring_size, dtype=np.float32),
-                        )
+                        # **NOTE** the ring buffer for the voltage should already be added by the readout
+                        # model_copy.add_egp(
+                        #    "RingV",
+                        #    "scalar*",
+                        #    np.empty(ring_size, dtype=np.float32),
+                        # )
 
                         model_copy.add_egp(
                             "Gradient",
@@ -659,17 +662,19 @@ class EventPropCompiler(Compiler):
                         # Is it possible that this is just to check if any readout mode OTHER than AvgVar or SumVar
                         # such as "spike_count" is used (which would definitely make no sense in the context of EventProp, because the gradients
                         # of the spike count with respect to spike times or voltages would be zero)?
-                        if isinstance(pop.neuron.readout, Var):
+                        if isinstance(pop.neuron.readout, VarTrial):
                             model_copy.prepend_sim_code(
                                 f"""
                                 const unsigned int timestep = min((int)(t / dt), {self.example_timesteps - 1});
                                 // **NOTE** The ordering of the indices might be wrong.
                                 // index for reversed time.
-                                unsigned int index = (({self.example_timesteps} - 1 - timestep) * num_batch * num_neurons) + (batch * num_neurons) + id;
+                                unsigned int index = batch * {self.example_timesteps} * num_neurons + (({self.example_timesteps} - 1 - timestep) * num_neurons) + id;
 
                                 if (Trial > 0) {{
                                     const scalar grad = Gradient[index];
-                                    drive = grad / (TauM * num_batch * {self.dt * self.example_timesteps});
+                                    drive = -grad;
+                                    // why is drive being scaled? I think in the manual case, this is not necessary.
+                                    //drive = -grad / (TauM * num_batch * {self.dt * self.example_timesteps});
                                 }}
                                 """
                             )
@@ -677,19 +682,20 @@ class EventPropCompiler(Compiler):
                             compile_state.add_neuron_reset_vars(
                                 pop, reset_vars, False, True
                             )
-                            # Add code to store voltages.
-                            model_copy.append_sim_code(
-                                """
-                                // index for forward time.
-                                // **NOTE** The ordering of the indices might be wrong.
-                                index = (timestep * num_batch * num_neurons) + (batch * num_neurons) + id;
-                                RingV[index] = V;
-                                """
-                            )
+
+                            # **NOTE** The code for storing the voltages should have been added by the readout
+                            # model_copy.append_sim_code(
+                            #    """
+                            #    // index for forward time.
+                            #    // **NOTE** The ordering of the indices might be wrong.
+                            #    index = (timestep * num_batch * num_neurons) + (batch * num_neurons) + id;
+                            #    RingV[index] = V;
+                            #    """
+                            # )
                         else:
                             raise NotImplementedError(
                                 "EventProp compiler with SetTargetGeneric loss only supports "
-                                "'Var' readouts"
+                                "'VarTrial' readouts"
                             )
 
                     # Otherwise, unsupported readout type

ml_genn/ml_genn/losses/manual_gradient.py
@@ -5,7 +5,7 @@ from ..utils.model import NeuronModel
 
 from typing import Callable
 
-from jax import grad
+from jax import grad, vmap
 
 
 class ManualGradient(Loss):
@@ -13,6 +13,11 @@ class ManualGradient(Loss):
         self.loss = loss_func
         # the provided loss function should take the input/predictions as the first argument.
         self.grad_loss = grad(loss_func)
+        # if we use self.grad_loss "as is", this can lead to gradients being interdependent
+        # between batches (which are represented by the 0-th axis in set_target).
+        # So we use jax.vmap to vectorize over the 0-th axis, which means that the losses/gradients
+        # are calculated independently across batches.
+        self.grad_loss_batch = vmap(self.grad_loss, in_axes=0, out_axes=0)
 
     def add_to_neuron(
         self, model: NeuronModel, shape, batch_size: int, example_timesteps: int
@@ -31,9 +36,22 @@ class ManualGradient(Loss):
 
         y_true = np.asarray(y_true)
 
+        # check if the number of dimensions match
+        assert y_true.ndim == len(
+            expected_shape
+        ), f"Expected {len(expected_shape)} axes, but got {y_true.ndim} for y_true."
+
+        batch_size_y_true = y_true.shape[0]
+
+        # check if the number of batches is smaller or equal to batch_size.
         assert (
-            y_true.shape == expected_shape
-        ), f"Expected shape {expected_shape}, got {y_true.shape} for y_true"
+            batch_size_y_true <= batch_size
+        ), f"Number of batches in y_true was {batch_size_y_true}, but must be less or equal than {batch_size}."
+
+        # check if the rest of the axes match
+        assert (
+            y_true.shape[1:] == expected_shape[1:]
+        ), f"Expected shape {expected_shape[1:]} for non-batch axes, got {y_true.shape[1:]} for y_true."
 
         genn_pop.extra_global_params["RingV"].pull_from_device()
 
@@ -41,7 +59,10 @@ class ManualGradient(Loss):
             genn_pop.extra_global_params["RingV"].view[:], expected_shape
         )
 
-        gradients = -self.grad_loss(voltages, y_true)
+        gradients = np.zeros(expected_shape)
+        gradients[:batch_size_y_true] = self.grad_loss_batch(
+            voltages[:batch_size_y_true], y_true
+        )
 
         genn_pop.extra_global_params["Gradient"].view[:] = gradients.flatten()
         genn_pop.extra_global_params["Gradient"].push_to_device()